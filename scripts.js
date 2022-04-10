9909a64755af28356855f7736e1095de29ee7b5d
Lucas <bazezimo42@gmail.com>
2022-04-10 19:04:13 -0300

    Versão 0.3 - Layout pronto

--- scripts.js
+++ scripts.js
@@ -1,3 +1,48 @@
 function comecarJogo() {
+    const qntCartas = Number(prompt("Com quantas cartas você quer jogar?\nDe 2 à 14"));
     
+    while(Number.isInteger(qntCartas)) {
+
+        if(isNaN(qntCartas)) {
+            alert("Insira um valor numérico INTEIRO de 2 à 14!");
+            return;
+        }
+
+        if(qntCartas > 14 || qntCartas < 1) {
+            alert("Quantidade inválida, tente novamente");
+            return;
+        }
+
+        if(qntCartas%2 !== 0 && qntCartas > 7) {
+            alert ("Quantidade inválida, tente novamente");
+            return;
+        }
+
+        if(qntCartas%2 !== 0) {
+            const erroCartas = prompt(`Você quis dizer: ${qntCartas} pares de cartas, ou seja, ${qntCartas*2}?\nResponda com SIM ou NÃO`);
+            if (erroCartas.toLowerCase() === "sim") {
+                alert(`Você ira jogar com ${qntCartas * 2} cartas, bom jogo.`);
+                return darCartas(qntCartas*2);
+            } else { 
+                alert ("Não foi possível entender como quer jogar, começe novamente");
+                return;
+            }
+        } else {
+            alert (`Você ira jogar com ${qntCartas} cartas, bom jogo`);
+            return darCartas(qntCartas);
+        }
+
+    }
+    alert("Insira um valor numérico INTEIRO de 2 à 14!");
+    return;
+}
+
+function darCartas(numero) {
+    const areaJogo = document.querySelector(".area-jogo");
+    document.querySelector(".pagina-inicial").classList.add("escondido");
+    document.querySelector(".pagina").classList.remove("escondido");
+    
+    for(let i = 0; i < numero; i++) {
+        areaJogo.innerHTML += `<div class="carta"><img src="/Projeto_04_parrotsCardGame/imagens/front.png" alt=""></div>`
+    }
 }
